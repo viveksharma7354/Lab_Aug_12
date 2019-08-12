b9b4c9cc3985e663a98e7d47753bf362afab05c1
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-12 15:23:39 +0530

    Initial Commit

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+/*
+* @author Vivek Sharma
+* @version 1.0
+ */
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+    int n= 3;
+    for (int i = 1 ; i<=10 ; i++)
+    {
+        System.out.println("");
+    }
+    }
+}
+
