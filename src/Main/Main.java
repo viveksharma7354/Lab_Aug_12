def9df1a9abd1a543b63382a5b8efdc4e3b575eb
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-12 15:59:43 +0530

    Main.java: added code of Solution For assignment 1

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,11 +9,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner S = new Scanner(System.in);
-        int n = S.nextInt();
-        if(n % 4 == 0 || n % 400 == 0){
-            System.out.println("Inputted Year Is a Leap Year.");
-        }
-        else System.out.println("Inputted Year Is Not a Leap Year.");
-        }
+        double speed, distance;
+        double gravity = 9.8 ;
+        double time = S.nextDouble();
+        speed = time * gravity ;
+        distance = (0.5 * gravity * time * time);
+        System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the release is " + distance);
+    }
 }
 
